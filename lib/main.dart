ef02f9158f88373d54c9c400208da456b51190b9
Ariel <ariel.ortega.zitle98@gmail.com>
2021-11-06 13:43:03 -0600

    Cambie el tema y se agregaron los assets
    Le agregue el color de la aplicacion y las imagenes a utilizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_retec/src/pages/tema/palette.dart';
 import 'package:flutter_retec/src/routes/routes.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final selection = Colors.green[400]!;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+        primarySwatch: Palette.kToDark,
+        //Colors.amber 
+      ),
       title: 'Retec App',
       initialRoute: '/LoginScreen',
       routes: getAplicationRoutes(),
--- lib/src/pages/login_screen.dart
+++ lib/src/pages/login_screen.dart
@@ -7,7 +7,10 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Image.asset('assets/fondo_login.jpg', fit: BoxFit.cover),
+        //title: Image.asset('assets/fondo_login.jpg', fit: BoxFit.cover)
         title: const Text('Login retec'),
+        //actionsIconTheme: ,
       ),
       body: Center(
         child: ElevatedButton(
